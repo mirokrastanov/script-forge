14e32285397262a12c5f0f3963fc8b936a77740c
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2025-03-08 19:34:37 +0200

    Update editor configuration

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -10,6 +10,7 @@ import { Clock, Code, MessageSquare, User } from "lucide-react";
 import { Editor } from "@monaco-editor/react";
 import { defineMonacoThemes, LANGUAGE_CONFIG } from "@/app/(root)/_constants";
 import CopyButton from "./_components/CopyButton";
+import Comments from "./_components/Comments";
 
 function SnippetDetailPage() {
     const snippetId = useParams().id;
@@ -83,14 +84,21 @@ function SnippetDetailPage() {
                                 readOnly: true,
                                 automaticLayout: true,
                                 scrollBeyondLastLine: false,
-                                padding: { top: 16 },
+                                padding: { top: 16, bottom: 20 },
                                 renderWhitespace: "selection",
                                 fontFamily: '"Fira Code", "Cascadia Code", Consolas, monospace',
                                 fontLigatures: true,
+                                scrollbar: {
+                                    alwaysConsumeMouseWheel: true, // switch for seemless auto-scrolling parent
+                                    verticalHasArrows: true,
+                                    horizontalHasArrows: true,
+                                    vertical: 'visible',
+                                },
                             }}
                         />
                     </div>
 
+                    <Comments snippetId={snippet._id} />
                 </div>
             </main>
         </div>
