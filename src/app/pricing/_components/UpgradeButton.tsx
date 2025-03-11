d7a2534cfd4a50299e62a63173f1345afa194e13
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2025-03-11 18:12:30 +0200

    Implement lemon squeezy payments into the upgrade button.

--- src/app/pricing/_components/UpgradeButton.tsx
+++ src/app/pricing/_components/UpgradeButton.tsx
@@ -1,6 +1,14 @@
+import { Zap } from "lucide-react";
+import Link from "next/link";
+
 export default function UpgradeButton() {
+    const CHEKOUT_URL = process.env.LEMON_CHECKOUT_URL || '';
 
     return (
-        <div>Upgrade to Pro</div>
+        <Link href={CHEKOUT_URL} className="inline-flex items-center justify-center gap-2 px-8 py-4 text-white 
+        bg-gradient-to-r from-blue-500 to-blue-600 rounded-lg hover:from-blue-600 hover:to-blue-700 transition-all">
+            <Zap className="w-5 h-5" />
+            Upgrade to Pro
+        </Link>
     );
 }
